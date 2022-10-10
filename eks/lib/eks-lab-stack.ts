5b1f07f1fadb28de49b3dc0905326eca78a0e36c
Javy de Koning <javy@amazon.nl>
2022-10-10 10:40:23 +0200

    Upgrading karpenter and eks blueprints

--- eks/lib/eks-lab-stack.ts
+++ eks/lib/eks-lab-stack.ts
@@ -1,8 +1,8 @@
-import { Stack, IResource, StackProps, aws_eks as eks } from 'aws-cdk-lib';
-import * as blueprints from '@aws-quickstart/eks-blueprints';
-import { Construct } from 'constructs';
-import { IVpc } from 'aws-cdk-lib/aws-ec2';
-import { IHostedZone } from 'aws-cdk-lib/aws-route53';
+import { Stack, IResource, StackProps, aws_eks as eks } from "aws-cdk-lib";
+import * as blueprints from "@aws-quickstart/eks-blueprints";
+import { Construct } from "constructs";
+import { IVpc } from "aws-cdk-lib/aws-ec2";
+import { IHostedZone } from "aws-cdk-lib/aws-route53";
 
 export interface EksLabStackProps extends StackProps {
   vpc: IVpc;
@@ -21,12 +21,12 @@ export class EksLabStack extends Stack {
         eks.ClusterLoggingTypes.AUDIT,
         eks.ClusterLoggingTypes.AUTHENTICATOR,
         eks.ClusterLoggingTypes.CONTROLLER_MANAGER,
-        eks.ClusterLoggingTypes.SCHEDULER
+        eks.ClusterLoggingTypes.SCHEDULER,
       ],
       fargateProfiles: {
         karpenter: {
-          fargateProfileName: 'karpenter',
-          selectors: [{ namespace: 'karpenter' }],
+          fargateProfileName: "karpenter",
+          selectors: [{ namespace: "karpenter" }],
         },
       },
     });
@@ -34,19 +34,31 @@ export class EksLabStack extends Stack {
     const addOns: Array<blueprints.ClusterAddOn> = [
       new blueprints.addons.VpcCniAddOn(),
       new blueprints.addons.KarpenterAddOn({
-        amiFamily: 'AL2',
-        version: '0.16.1',
-        provisionerSpecs: {
-          'karpenter.sh/capacity-type': ['spot'],
-          "kubernetes.io/arch": ["amd64","arm64"],
-          "topology.kubernetes.io/zone": ["eu-west-1a", "eu-west-1b"],
-          //"node.kubernetes.io/instance-type": ["m6i.large", "m5.large"]
-        },
+        amiFamily: "AL2",
+        version: "0.16.2",
+        requirements: [
+          {
+            key: "karpenter.sh/capacity-type",
+            op: "In",
+            vals: ["spot"],
+          },
+          {
+            key: "kubernetes.io/arch",
+            op: "In",
+            vals: ["amd64", "arm64"],
+          },
+          {
+            key: "topology.kubernetes.io/zone",
+            op: "In",
+            vals: ["eu-west-1a", "eu-west-1b"],
+          },
+        ],
+        consolidation: { enabled: true },
         subnetTags: {
-          'karpenter.sh/discovery': 'eks-lab',
+          "karpenter.sh/discovery": "eks-lab",
         },
         securityGroupTags: {
-          'kubernetes.io/cluster/eks-cluster': 'owned'
+          "kubernetes.io/cluster/eks-cluster": "owned",
         },
       }),
       new blueprints.addons.AwsLoadBalancerControllerAddOn(),
@@ -60,18 +72,25 @@ export class EksLabStack extends Stack {
       //new blueprints.addons.MetricsServerAddOn(),
     ];
 
-    const resourceProviders = new Map<string,blueprints.ResourceProvider<IResource>>([
+    const resourceProviders = new Map<
+      string,
+      blueprints.ResourceProvider<IResource>
+    >([
       [
         blueprints.GlobalResources.Vpc,
         new blueprints.DirectVpcProvider(props.vpc),
-      ],[
-        props.zone.zoneName,
-        new blueprints.ImportHostedZoneProvider(props.zone.hostedZoneId)
       ],
       [
-        'cert',
-        new blueprints.ImportCertificateProvider(props.certificateArn, 'wildcardCert')
-      ]
+        props.zone.zoneName,
+        new blueprints.ImportHostedZoneProvider(props.zone.hostedZoneId),
+      ],
+      [
+        "cert",
+        new blueprints.ImportCertificateProvider(
+          props.certificateArn,
+          "wildcardCert"
+        ),
+      ],
     ]);
 
     new blueprints.EksBlueprint(
@@ -80,7 +99,7 @@ export class EksLabStack extends Stack {
         addOns,
         clusterProvider,
         resourceProviders,
-        id: 'eks-cluster',
+        id: "eks-cluster",
       },
       props
     );
