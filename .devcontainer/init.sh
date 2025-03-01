a8365f2eb28a52410159708bbd9def58ec635e0c
Javy de Koning <javydekoning+github@gmail.com>
2025-03-01 20:54:12 +0000

    Clean-up

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 set -euo pipefail
-apt-get update && apt-get install curl jq git zsh python3-pip -y
+apt-get update && apt-get install curl jq git zsh python3-pip unzip -y
 
-# cfn-lint
-pip install cfn-lint
+# Install terraform
+wget https://releases.hashicorp.com/terraform/1.11.0/terraform_1.11.0_linux_amd64.zip
+unzip terraform_1.11.0_linux_amd64.zip
+chmod a+x /usr/bin/terraform
+
+# Configure git
+git config --global user.email "javydekoning+github@gmail.com"
+git config --global user.name "Javy de Koning
