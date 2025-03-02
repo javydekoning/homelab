964f02753f65d0778c620fe8eb1faba6ff568c66
Javy de Koning <javydekoning+github@gmail.com>
2025-03-02 07:51:54 +0000

    Updating devcontainer

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,12 +1,25 @@
-#!/usr/bin/bash
+#!/bin/bash
 set -euo pipefail
-apt-get update && apt-get install curl jq git zsh python3-pip unzip -y
+
+sudo apt-get update && sudo apt-get install curl jq git zsh python3-pip unzip -y
 
 # Install terraform
-wget https://releases.hashicorp.com/terraform/1.11.0/terraform_1.11.0_linux_amd64.zip
-unzip terraform_1.11.0_linux_amd64.zip
-chmod a+x /usr/bin/terraform
+cd /tmp
+# get latest version from https://github.com/hashicorp/terraform/releases
+LATEST_VERSION=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | jq -r '.tag_name')
+# strip leading 'v' from LATEST_VERSION
+LATEST_VERSION=${LATEST_VERSION#v}
+# download, move to /usr/bin, make executable
+wget https://releases.hashicorp.com/terraform/${LATEST_VERSION}/terraform_${LATEST_VERSION}_linux_amd64.zip
+unzip -o terraform_${LATEST_VERSION}_linux_amd64.zip
+sudo mv terraform /usr/bin/
+sudo chmod a+x /usr/bin/terraform
 
 # Configure git
 git config --global user.email "javydekoning+github@gmail.com"
 git config --global user.name "Javy de Koning"
+
+# install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
