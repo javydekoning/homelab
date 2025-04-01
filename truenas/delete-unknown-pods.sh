b0edec274127593691b3273f4bee42afd3bbf592
javydekoning <javydekoning+github@gmail.com>
2025-04-01 12:48:27 +0000

    Fixing linter

--- truenas/delete-unknown-pods.sh
+++ truenas/delete-unknown-pods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-kubectl get pods --all-namespaces -o json | jq -r '.items[] | select(.status.containerStatuses[].state.terminated.reason=="Unknown") | [.metadata.namespace, .metadata.name] | @tsv' | while read namespace name; do
-  kubectl delete pod $name -n $namespace
+kubectl get pods --all-namespaces -o json | jq -r '.items[] | select(.status.containerStatuses[].state.terminated.reason=="Unknown") | [.metadata.namespace, .metadata.name] | @tsv' | while read -r namespace name; do
+  kubectl delete pod "$name" -n "$namespace"
 done
