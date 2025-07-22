d397606ba43092b860bb1e8fb5c470dfd16c4923
javydekoning <javydekoning+github@gmail.com>
2025-07-22 10:02:55 +0000

    Fixing linter

--- dotfiles/install_kubecolor.sh
+++ dotfiles/install_kubecolor.sh
@@ -4,10 +4,10 @@
 LATEST_RELEASE_URL="https://github.com/kubecolor/kubecolor/releases/latest"
 
 # Use curl to get the URL of the latest release quietly
-REDIRECT_URL=$(curl -sL -o /dev/null -w %{url_effective} $LATEST_RELEASE_URL)
+REDIRECT_URL=$(curl -sL -o /dev/null -w "%{url_effective}" "$LATEST_RELEASE_URL")
 
 # Extract the tag (release version) from the URL
-TAG=$(basename $REDIRECT_URL)
+TAG=$(basename "$REDIRECT_URL")
 
 # Strip the leading 'v' from the tag
 TAG=${TAG#v}
@@ -17,10 +17,10 @@ TAG=${TAG#v}
 DOWNLOAD_URL="https://github.com/kubecolor/kubecolor/releases/download/v${TAG}/kubecolor_${TAG}_linux_amd64.tar.gz"
 echo "Downloading from: ${DOWNLOAD_URL}"
 # Download the release tarball quietly
-curl -sL -o /tmp/kubecolor_${TAG}_Linux_x86_64.tar.gz $DOWNLOAD_URL
+curl -sL -o "/tmp/kubecolor_${TAG}_Linux_x86_64.tar.gz" "$DOWNLOAD_URL"
 
 # Extract the tarball quietly
-tar -xzf /tmp/kubecolor_${TAG}_Linux_x86_64.tar.gz -C /tmp
+tar -xzf "/tmp/kubecolor_${TAG}_Linux_x86_64.tar.gz" -C /tmp
 
 # Move the binary to the correct location
 sudo mv /tmp/kubecolor /usr/bin/kubecolor
